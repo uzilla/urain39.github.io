6dbcd4b23b552fd4bb9377b7d733663b217f088b
urain39 <urain39@qq.com>
2020-03-11 05:31:29 +0800

    feat: try to add a tiny notice

---
+++ assets/js/tinynotice.js
@@ -0,0 +1,23 @@
+(function() {
+	var date = new Date();
+	var pathname = self.location.pathname
+	var postYear = parseInt(pathname.slice(1, 5));
+	var postMonth = parseInt(pathname.slice(6, 8));
+	if ((date.getFullYear() - postYear) * 12 +
+		(date.getMonth() + 1 - postMonth) < 6)
+		return; // 小于6个月，非过时内容
+	var notice = document.createElement('div');
+	notice.style['top'] = '0';
+	notice.style['left'] = '0';
+	notice.style['width'] = '100%';
+	notice.style['text-align'] = 'center';
+	notice.style['background'] = '#ffee00';
+	notice.innerHTML = '<h2>注意：页面内容可能过时或不正确，请谨慎参考。</h2>';
+	var firstChild = document.body.firstChild;
+	document.body.insertBefore(notice, firstChild);
+	setTimeout(function () {
+		notice.style['display'] = 'none';
+	}, 1000 * 10);
+})();
+
+
